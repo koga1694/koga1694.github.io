a7f2d302d05fcd9a8a823fcb12f29ef90129a890
koga1694 <kweon0323@gmail.com>
2026-04-03 16:52:26 +0900

    Add .nojekyll file and update config for GitHub Pages

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,9 @@ export default function (eleventyConfig) {
   // CSS 파일은 그대로 복사
   eleventyConfig.addPassthroughCopy("src/css");
 
+  // .nojekyll 파일을 추가하여 Jekyll 처리를 비활성화
+  eleventyConfig.addPassthroughCopy("src/.nojekyll");
+
   // 프로젝트 섹션을 컬렉션으로 등록
   eleventyConfig.addCollection("doyourself", (collectionApi) => {
     return collectionApi
